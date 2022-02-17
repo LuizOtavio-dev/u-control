44af21af959e5c62e4cdb5415d8d9a0aa4dbd9a8
Luiz Otavio <luiz.otavio273@gmail.com>
2022-02-17 00:27:15 -0300

    feat: create function for input and button

--- frontend/src/components/body.jsx
+++ frontend/src/components/body.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './body.css';
 
 function Body() {
+  const [task, setTask] = useState("");
+  const [itemsList, setItemsList] = useState([])
+
+  function handleChangeInput({ target: {value} }) {
+    const inputTask = value
+  
+    setTask(inputTask)
+  }
+
+  function handleAddItemToList(event) {
+    event.preventDefault();
+    
+    if(task) {
+      setItemsList([...itemsList, task]);    
+  
+      setTask("")
+    }
+  }
+
   return(
     <div>
       <h1>"Com organização e tempo, acha-se o segredo de fazer tudo e bem feito.” -Pitágoras</h1>
       <form>
         <label>
-          <input type="text" placeholder="Adicione uma tarefa" />
+          <input type="text" placeholder="Adicione uma tarefa" onChange={handleChangeInput} value={task} />
         </label>
-        <button type="submit">Adicionar</button>
+        <button type="submit" onClick={handleAddItemToList}>Adicionar</button>
       </form>
       <ul>
         <li>Item</li>
